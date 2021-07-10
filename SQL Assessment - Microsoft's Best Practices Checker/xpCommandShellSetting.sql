27a1200f58287f78b60423134e905924a4a3e2a1
sqlworldwide <taiobmdjamshed@yahoo.com>
2021-07-10 10:49:26 -0400

    Add files via upload

---
+++ SQL Assessment - Microsoft's Best Practices Checker/xpCommandShellSetting.sql
@@ -0,0 +1,12 @@
+-- To allow advanced options to be changed.  
+EXECUTE sp_configure 'show advanced options', 1;  
+GO  
+-- To update the currently configured value for advanced options.  
+RECONFIGURE;  
+GO  
+-- To enable the feature.  
+EXECUTE sp_configure 'xp_cmdshell', 1;  
+GO  
+-- To update the currently configured value for this feature.  
+RECONFIGURE;  
+GO  
