86a444a50efbe38ea478b460ddfd158c1d82a881
sqlworldwide <taiobmdjamshed@yahoo.com>
2022-03-02 15:01:33 -0500

    new presentation

---
+++ Analyzing Azure Monitor Log data in Azure Data Studio/Setup2.sql
@@ -0,0 +1,103 @@
+/*
+Setup2.sql
+Written by Taiob  Ali
+SqlWorldWide.com
+*/
+
+/*
+Connect to sqlbitsdemoservertaiob.database.windows.net
+Change database context to sqlbitsdemodatabase
+*/
+
+DROP TABLE IF EXISTS dbo.dt_Employees;
+GO
+CREATE TABLE dbo.dt_Employees (
+    EmpId INT IDENTITY,
+    EmpName VARCHAR(16),
+    Phone VARCHAR(16)
+);
+GO
+INSERT INTO dbo.dt_Employees (EmpName, Phone)
+VALUES ('Martha', '800-555-1212'), ('Jimmy', '619-555-8080');
+GO
+DROP TABLE IF EXISTS dbo.dt_Suppliers;
+GO
+CREATE TABLE dbo.dt_Suppliers(
+    SupplierId INT IDENTITY,
+    SupplierName VARCHAR(64),
+    Fax VARCHAR(16)
+);
+GO
+INSERT INTO dbo.dt_Suppliers (SupplierName, Fax)
+VALUES ('Acme', '877-555-6060'), ('Rockwell', '800-257-1234');
+GO
+
+/*
+Run this in current window
+*/
+
+BEGIN TRAN;
+UPDATE dbo.dt_Employees
+SET EmpName = 'Mary'
+WHERE EmpId = 1;
+
+/*
+Open another window and run this
+*/
+
+BEGIN TRAN;
+UPDATE dbo.dt_Suppliers
+SET Fax = N'555-1212'
+WHERE SupplierId = 1;
+
+UPDATE dbo.dt_Employees
+SET Phone = N'555-9999'
+WHERE EmpId = 1;
+
+/*
+Continue here
+*/
+
+UPDATE dbo.dt_Suppliers
+SET Fax = N'555-1212'
+WHERE SupplierId = 1;
+
+/*
+You will get a deadlock message in one of the window
+Commit 
+Clean up
+*/
+COMMIT TRAN;
+
+DROP TABLE IF EXISTS dbo.dt_Suppliers;
+DROP TABLE IF EXISTS dbo.dt_Employees;
+GO
+/*
+ After about 5~7 minuetes of running this deadlock should fire alert  that was configured by DemoKustoQueryLanguage.ipynb file
+*/
+
+/*
+After dropping the two tables from above demo  
+Use the query below to find the drop event
+*/
+
+/*
+This is Kusto Query  
+
+Ref: https://techcommunity.microsoft.com/t5/azure-database-support-blog/azure-sql-db-and-log-analytics-better-together-part-3-query/ba-p/1034222
+
+Who DROPPED my table?
+let ServerName = "sqlalertdemoserver";
+let DBName = "sqlalertdemodatabase";
+AzureDiagnostics
+| where TimeGenerated >= ago(1d)
+| where LogicalServerName_s =~ ServerName
+| where database_name_s =~ DBName
+| where Category =~ "SQLSecurityAuditEvents"
+| where action_name_s in ("BATCH COMPLETED", "RPC COMPLETED")
+| where statement_s contains "DROP" or statement_s contains "TRUNCATE" 
+| project TimeGenerated, event_time_t, LogicalServerName_s, database_name_s, succeeded_s, session_id_d, action_name_s,
+            client_ip_s, session_server_principal_name_s , database_principal_name_s, statement_s, additional_information_s, application_name_s
+| top 1000 by TimeGenerated desc
+
+*/
---
+++ Analyzing Azure Monitor Log data in Azure Data Studio/Setup3.sql
@@ -0,0 +1,105 @@
+/*
+Setup3.sql
+Written by Taiob Ali
+SqlWorldWide.com
+*/
+
+/*
+Connect to server sqlbitsdemoservertaiob.database.windows.net
+Change database context to sqlbitsdemodatabase
+With Azure SQL Database cannot use 'USE DBName' statement
+Create an empty table
+*/
+
+SET NOCOUNT ON;
+GO
+DROP TABLE IF EXISTS [dbo].[StressTestTable] ;
+GO
+
+CREATE TABLE [dbo].[StressTestTable] (
+  [StressTestTableID] [BIGINT] IDENTITY(1,1) NOT NULL,
+  [ColA] char(2000) NOT NULL,
+  [ColB] char(2000) NOT NULL,
+  [ColC] char(2000) NOT Null,
+  [ColD] char(2000) NOT Null,
+  CONSTRAINT [PK_StressTestTable] PRIMARY KEY CLUSTERED 
+  (
+	  [StressTestTableID] ASC
+  )
+);
+GO
+
+/*
+Create store procedures
+*/
+DROP PROCEDURE IF EXISTS [dbo].[p_StressTestTable_ins];
+GO
+
+SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER OFF;
+GO
+
+CREATE PROCEDURE [dbo].[p_StressTestTable_ins] 
+AS
+SET NOCOUNT ON 
+DECLARE @l_cola char(1) , @l_colb char(1) ,@l_colc char(1) ,@l_cold char(1) , @p_seed int
+SELECT  @p_seed=ABS(CHECKSUM(NewId())) % 127 
+SELECT @l_cola =char(@p_seed) , @l_colb =char(@p_seed) ,@l_colc =char(@p_seed) ,@l_cold =char(@p_seed) 
+
+INSERT INTO [dbo].[StressTestTable]
+  (
+		[ColA],
+    [ColB],
+    [ColC],
+    [ColD]
+	)
+  VALUES
+	(
+	  REPLICATE(@l_cola,2000),
+    REPLICATE(@l_colb,2000),
+    REPLICATE(@l_colc,2000),
+    REPLICATE(@l_cold,2000)
+	)
+SET NOCOUNT OFF
+RETURN 0;
+GO
+
+
+DROP PROCEDURE IF EXISTS [dbo].[p_StressTestTable_upd];
+GO
+
+CREATE PROCEDURE [dbo].[p_StressTestTable_upd] 
+AS
+SET NOCOUNT ON 
+
+DECLARE @l_cola char(1) , @l_colb char(1) ,@l_colc char(1) ,@l_cold char(1) , @p_seed int, @Upper int, @Lower int,@PK_ID bigint 
+SELECT  @p_seed=ABS(CHECKSUM(NewId())) % 127 -- check asciitable.com 
+SELECT @l_cola =char(@p_seed) , @l_colb =char(@p_seed) ,@l_colc =char(@p_seed) ,@l_cold =char(@p_seed) 
+SELECT @Lower = (SELECT TOP 1 StressTestTableId FROM [StressTestTable] WITH(NOLOCK) ORDER BY StressTestTableId ASC)
+SELECT @Upper = (SELECT TOP 1 StressTestTableId FROM [StressTestTable] WITH(NOLOCK) ORDER BY StressTestTableId DESC)
+
+---http://kaniks.blogspot.com/search/label/generate%20random%20number%20from%20t-sql
+SELECT @PK_ID = Round(((@Upper - @Lower -1) * Rand() + @Lower), 0)
+
+UPDATE [dbo].[StressTestTable]
+  SET [ColA] = REPLICATE(@l_cola,2000),
+      [ColB] = REPLICATE(@l_cola,2000),
+      [ColC] = REPLICATE(@l_cola,2000),
+      [ColD] = REPLICATE(@l_cola,2000)
+WHERE StressTestTableId = @PK_ID
+
+SET NOCOUNT OFF
+RETURN 0;   
+GO
+
+/*
+Run this from query stress tool
+EXEC p_StressTestTable_ins
+EXEC p_StressTestTable_upd
+https://github.com/ErikEJ/SqlQueryStress
+Chose server name, database
+Set number of threads = 20
+Number of iterations = 100,000
+Click GO
+*/
